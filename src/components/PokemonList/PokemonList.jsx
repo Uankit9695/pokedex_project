7e25405662700f4b0d4aa866fa7816b834463926
Ankit <uankitbecse123@gmail.com>
2024-08-24 05:16:19 +0530

    added css to pokedex

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,52 +1,58 @@
-
 import { useEffect, useState } from "react";
 import axios from "axios";
-import './PokemonList.css'
+import "./PokemonList.css";
 import Pokemon from "../Pokemon/Pokemon";
-function PokemonList(){
+function PokemonList() {
+  const [pokemonList, setPokemosList] = useState([]);
+  const [isLoding, setIsLoding] = useState(true);
 
-    const [pokemonList , setPokemosList]=useState([]);
-    const [isLoding , setIsLoding]= useState(true);
+  const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon";
 
-    const POKEDEX_URL='https://pokeapi.co/api/v2/pokemon';
+  async function dawnlodPkemons() {
+    const response = await axios.get(POKEDEX_URL);
+    const pokemonsResult = response.data.results;
+    console.log(response.data);
+    const pokemonResultPromise = pokemonsResult.map((pokemon) =>
+      axios.get(pokemon.url)
+    );
+    const pokemonData = await axios.all(pokemonResultPromise);
+    console.log(pokemonData);
+    const pokeListResult = pokemonData.map((pokeData) => {
+      const pokemon = pokeData.data;
+      return {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: pokemon.sprites.other
+          ? pokemon.sprites.other.dream_world.front_default
+          : pokemon.sprites.front_shiny,
+        types: pokemon.types,
+      };
+    });
+    console.log(pokeListResult);
+    setPokemosList(pokeListResult);
 
-    async function dawnlodPkemons() {
-        const response = await axios.get(POKEDEX_URL);
-        const pokemonsResult= response.data.results;
-        console.log(response.data);
-        const pokemonResultPromise=  pokemonsResult.map((pokemon)=>axios.get(pokemon.url));
-        const pokemonData=await axios.all(pokemonResultPromise);
-        console.log(pokemonData);
-        const pokeListResult=pokemonData.map((pokeData)=>{
-            const pokemon= pokeData.data;
-            return{
-                id:pokemon.id,
-                name:pokemon.name, 
-                image:(pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny,
-                types:pokemon.types }
-        });
-        console.log(pokeListResult);
-        setPokemosList(pokeListResult);
-       
-        setIsLoding(false);
-    }
+    setIsLoding(false);
+  }
 
-   
+  useEffect(() => {
+    dawnlodPkemons();
+  }, []);
 
-    useEffect(()=>{
-        dawnlodPkemons();
-    },[]);
-
-    return (
-        <div className="pokemon-list-wrapper">
-            <div>pokemon list</div>
-            {(isLoding)? 'Loading...':
-                pokemonList.map((p)=> <Pokemon name={p.name} image={p.image} key={p.id}/>)
-            }
-
-        
-        </div>
-    )
-
+  return (
+    <div className="pokemon-list-wrapper">
+      
+      <div className="pokemon-wrapper">
+        {isLoding
+          ? "Loading..."
+          : pokemonList.map((p) => (
+              <Pokemon name={p.name} image={p.image} key={p.id} />
+            ))}
+      </div>
+      <div className="controls">
+        <button>Per</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
 }
 export default PokemonList;
